33395ab4c986f8cc7274ed2dca3061cc5a3f6c00
Ike-weber <harshgaikwad182@gmail.com>
2026-06-29 22:36:14 +0100

    v1.3.1: use downloaded Prysm binaries directly in start-all.sh
    - Replace ./prysm.sh calls with ./beacon-chain-v5.3.2 and ./validator-v5.3.2
    - Export PRYSM_ALLOW_UNVERIFIED_BINARIES=1
    - Fixes JWT auth mismatch caused by prysm.sh downloading wrong Prysm version

--- start-all.sh
+++ start-all.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 export USE_PRYSM_VERSION=v5.3.2
+export PRYSM_ALLOW_UNVERIFIED_BINARIES=1
+
+PRYSM_VERSION="v5.3.2"
+BEACON="./beacon-chain-${PRYSM_VERSION}"
+VALIDATOR="./validator-${PRYSM_VERSION}"
 
 # Start Geth nodes
 ./geth-1.17.4 --datadir node1 --port 30301 --http --http.port 8541 --http.api eth,net,engine,admin --authrpc.port 8551 --authrpc.jwtsecret jwt.hex --syncmode full --networkid 12345 --ipcdisable >> logs/geth1.log 2>&1 &
@@ -22,7 +27,7 @@ curl -s -X POST -H "Content-Type: application/json" --data "{\"jsonrpc\":\"2.0\"
 echo "Geth peered"
 
 # Start beacon1
-./prysm.sh beacon-chain --datadir beacon1 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0x8B0681dBD724dcaC48b433e9df8A220D47C94a19 --execution-endpoint http://localhost:8551 --rpc-port 4000 --grpc-gateway-port 3500 --p2p-tcp-port 13000 --p2p-udp-port 12000 --bootstrap-node= >> logs/beacon1.log 2>&1 &
+$BEACON --datadir beacon1 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0x8B0681dBD724dcaC48b433e9df8A220D47C94a19 --execution-endpoint http://localhost:8551 --rpc-port 4000 --grpc-gateway-port 3500 --p2p-tcp-port 13000 --p2p-udp-port 12000 --bootstrap-node= >> logs/beacon1.log 2>&1 &
 echo "Beacon1 started"
 
 # Wait for beacon1 API to be ready (retry up to 30s)
@@ -40,15 +45,15 @@ if [ -z "$BEACON1_PEER" ]; then
 fi
 
 # Start beacon2/3 with correct bootstrap
-./prysm.sh beacon-chain --datadir beacon2 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0xC4d87b80780117F805D620c4FF88e5380699dB41 --execution-endpoint http://localhost:8552 --rpc-port 4001 --grpc-gateway-port 3501 --p2p-tcp-port 13001 --p2p-udp-port 12001 --bootstrap-node=/ip4/127.0.0.1/tcp/13000/p2p/$BEACON1_PEER >> logs/beacon2.log 2>&1 &
-./prysm.sh beacon-chain --datadir beacon3 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0x2F54526037527688d3b2DEFA3a0B1F4CAf78dB8F --execution-endpoint http://localhost:8553 --rpc-port 4002 --grpc-gateway-port 3502 --p2p-tcp-port 13002 --p2p-udp-port 12002 --bootstrap-node=/ip4/127.0.0.1/tcp/13000/p2p/$BEACON1_PEER >> logs/beacon3.log 2>&1 &
+$BEACON --datadir beacon2 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0xC4d87b80780117F805D620c4FF88e5380699dB41 --execution-endpoint http://localhost:8552 --rpc-port 4001 --grpc-gateway-port 3501 --p2p-tcp-port 13001 --p2p-udp-port 12001 --bootstrap-node=/ip4/127.0.0.1/tcp/13000/p2p/$BEACON1_PEER >> logs/beacon2.log 2>&1 &
+$BEACON --datadir beacon3 --min-sync-peers 0 --genesis-state genesis.ssz --interop-eth1data-votes --chain-config-file config.yaml --contract-deployment-block 0 --chain-id 12345 --accept-terms-of-use --jwt-secret jwt.hex --suggested-fee-recipient 0x2F54526037527688d3b2DEFA3a0B1F4CAf78dB8F --execution-endpoint http://localhost:8553 --rpc-port 4002 --grpc-gateway-port 3502 --p2p-tcp-port 13002 --p2p-udp-port 12002 --bootstrap-node=/ip4/127.0.0.1/tcp/13000/p2p/$BEACON1_PEER >> logs/beacon3.log 2>&1 &
 echo "Beacon2/3 started"
 sleep 10
 
 # Start validators
-./prysm.sh validator --datadir beacon1/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 0 --beacon-rest-api-provider http://localhost:3500 >> logs/validator1.log 2>&1 &
-./prysm.sh validator --datadir beacon2/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 1 --beacon-rest-api-provider http://localhost:3501 >> logs/validator2.log 2>&1 &
-./prysm.sh validator --datadir beacon3/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 2 --beacon-rest-api-provider http://localhost:3502 >> logs/validator3.log 2>&1 &
+$VALIDATOR --datadir beacon1/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 0 --beacon-rest-api-provider http://localhost:3500 >> logs/validator1.log 2>&1 &
+$VALIDATOR --datadir beacon2/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 1 --beacon-rest-api-provider http://localhost:3501 >> logs/validator2.log 2>&1 &
+$VALIDATOR --datadir beacon3/validator --accept-terms-of-use --chain-config-file config.yaml --interop-num-validators 1 --interop-start-index 2 --beacon-rest-api-provider http://localhost:3502 >> logs/validator3.log 2>&1 &
 
 echo "All started"
 ps aux | grep -E "geth|prysm|beacon|validator" | grep -v grep | wc -l
